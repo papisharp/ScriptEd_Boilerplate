41ce996c51e96b16663683ffaffb43e679267799
Divy Tolia <divystolia@gmail.com>
2016-02-08 10:48:01 -0500

    Added structure
    Basic Html, css, and js structure added

---
+++ _base/static/js/base.js
@@ -0,0 +1,15 @@
+var //Any Name for variable
+    =
+  {
+
+  };
+
+//Functions
+  function myFunction(x,y) {
+      return x+y; // The function returns the sum of x and y
+  }
+
+//buttons
+function myButton() {
+    document.getElementById("container").innerHTML = "Hello World";
+}
